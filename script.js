af87044cb8ff2ffdfe43af76c07eb8e7ddad77e4
njahnavi24 <jahnavinagavalli24@gmail.com>
2026-09-24 15:18:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let i = 0
+let circles = document.querySelectorAll('.circle')
+let prev = document.getElementById('prev')
+let next = document.getElementById('next')
+
+prev.addEventListener("click",()=>{
+	i++
+	circles[i].classList.add('active')
+	prev.disabled = false
+
+	if(i === 4) next.disabled = true
+})
+
+next.addEventListener("click",()=>{
+	circles[i].classList.remove('active')
+	i--
+	next.disabled = false
+
+	if(i === 0) prev.disabled = true
+})
