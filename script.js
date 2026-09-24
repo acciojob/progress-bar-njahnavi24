5be38eca65054308e29c095e1b0666a27993ae44
njahnavi24 <jahnavinagavalli24@gmail.com>
2026-09-24 15:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,19 +5,23 @@ let prev = document.getElementById('prev')
 let next = document.getElementById('next')
 
 next.addEventListener("click",()=>{
+	if(i < circles.length-1){
 	circles[i].classList.remove('active')
 	i++
 	circles[i].classList.add('active')
 	prev.disabled = false
 
 	if(i === circles.length - 1) next.disabled = true
+	}
 })
 
 prev.addEventListener("click",()=>{
+	if(i > 0){
 	circles[i].classList.remove('active')
 	i--
 	circles[i].classList.add('active')
 	next.disabled = false
 
 	if(i === 0) prev.disabled = true
+	}
 })
